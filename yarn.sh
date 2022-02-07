b341e4f77e05413a75b0fce387939ab34f4e7806
ittp <61617660+ittp@users.noreply.github.com>
2022-02-07 09:57:11 +0300

    Update yarn.sh

--- yarn.sh
+++ yarn.sh
@@ -1,17 +1,18 @@
-#!/bin/env bash
+#!/bin/env sh
 
-# curl -fS https://raw.githubusercontent.com/ittp/install/main/yarn.sh | bash -
+# wget https://raw.githubusercontent.com/ittp/install/main/yarn.sh && chmod +x yarn.sh && ./yarn.sh
 
 add_sources() {
-  local keyring=/usr/share/keyrings/yarnkey.gpg", 
-  url="https://dl.yarnpkg.com/debian", 
-  list="/etc/apt/sources.list.d/yarn.list"
+  local keyring=/usr/share/keyrings/yarnkey.gpg, 
+  url=https://dl.yarnpkg.com/debian, 
+  list=/etc/apt/sources.list.d/yarn.list
   
-  # echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" |  tee /etc/apt/sources.list.d/yarn.list
-  echo "deb [signed-by=$keyring] $url stable main" |  tee $list
+  echo "deb [signed-by=${keyring}] ${url} stable main" |  tee ${list}
+  
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor |  tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 
 }
+
 install() {
   local pkg=$1
   apt-get update && apt-get install $pkg
